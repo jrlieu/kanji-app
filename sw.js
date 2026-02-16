f4ee8e5a35897b80e8433cbca7744d1daca725a1
jrlieu <clmcofficial@gmail.com>
2026-02-16 11:08:22 +0800

    Refactor service worker for cache management

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kanji-dict-v2'; // Increment this (v1 -> v2) to force a reset
+const CACHE_NAME = 'kanji-dict-v2'; // Increment this every time you push a change
 const urlsToCache = [
   './',
   './index.html',
@@ -6,43 +6,39 @@ const urlsToCache = [
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
 ];
 
-// 1. Install: Force the new worker to take over immediately
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
+  self.skipWaiting(); // Force activation
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// 2. Activate: Clean up any old caches from previous versions
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
         if (key !== CACHE_NAME) {
-          console.log('[Service Worker] Removing old cache', key);
           return caches.delete(key);
         }
       }));
-    }).then(() => self.clients.claim()) // Take control of all open tabs immediately
+    }).then(() => self.clients.claim())
   );
 });
 
-// 3. Fetch: Network First, then Cache
+// NETWORK FIRST STRATEGY
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request)
       .then((response) => {
-        // If network works, clone it into the cache and return
-        return caches.open(CACHE_NAME).then((cache) => {
-          cache.put(e.request, response.clone());
-          return response;
+        // If internet works, update the cache with new version
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, resClone);
         });
+        return response;
       })
       .catch(() => {
-        // If network fails (offline), use the cache
+        // If offline, use cache
         return caches.match(e.request);
       })
   );
