8af42dea6991f6ed9aabc5599bca3423f79311a9
jrlieu <clmcofficial@gmail.com>
2026-02-16 08:29:37 +0800

    Upgrade service worker to version 2
    Updated service worker to version 2 with improved caching strategy.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kanji-dict-v1';
+const CACHE_NAME = 'kanji-dict-v2'; // Increment this (v1 -> v2) to force a reset
 const urlsToCache = [
   './',
   './index.html',
@@ -6,6 +6,7 @@ const urlsToCache = [
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
 ];
 
+// 1. Install: Force the new worker to take over immediately
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(
@@ -15,22 +16,34 @@ self.addEventListener('install', (e) => {
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
-});
-
+// 2. Activate: Clean up any old caches from previous versions
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
         if (key !== CACHE_NAME) {
+          console.log('[Service Worker] Removing old cache', key);
           return caches.delete(key);
         }
       }));
-    })
+    }).then(() => self.clients.claim()) // Take control of all open tabs immediately
+  );
+});
+
+// 3. Fetch: Network First, then Cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((response) => {
+        // If network works, clone it into the cache and return
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => {
+        // If network fails (offline), use the cache
+        return caches.match(e.request);
+      })
   );
 });
